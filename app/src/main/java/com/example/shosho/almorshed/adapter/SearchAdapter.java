00140baa1dd3115b70dd5387e3c2925b53022d18
AlshimaaRifaat <alshimaa.rifaat71@gmail.com>
2019-01-04 21:32:18 +0200

    search is done + design + share

--- app/src/main/java/com/example/shosho/almorshed/adapter/SearchAdapter.java
+++ app/src/main/java/com/example/shosho/almorshed/adapter/SearchAdapter.java
@@ -1,11 +1,13 @@
 package com.example.shosho.almorshed.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.shosho.almorshed.R;
@@ -36,6 +38,17 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
         holder.meaning.setText( quranList.get( position ).getWord1() +" _ "+quranList.get( position ).getMeaning() );
         holder.part.setText(" ( " + quranList.get( position ).getPart() + " ) " );
 
+        holder.share.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "كلمة "+quranList.get( position ).getWord1()+" ظهرت في: "+ quranList.get( position ).getSouret()+" "+quranList.get( position ).getPart()+ " ومعناها: "+quranList.get( position ).getMeaning();
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_TEXT, message);
+                context.startActivity(Intent.createChooser(share, "نشر "+ quranList.get( position ).getWord2()+" الي "));
+            }
+        } );
+
 
     }
 
@@ -46,11 +59,13 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView word1,meaning,sourt,part;
+        private ImageView  share;
         public ViewHolder(View itemView) {
             super( itemView );
             meaning=itemView.findViewById( R.id.row_search_meaning );
             sourt=itemView.findViewById( R.id.row_search_sourt );
             part=itemView.findViewById( R.id.row_search_part );
+            share=itemView.findViewById( R.id.row_search_icon_share );
         }
     }
 }
