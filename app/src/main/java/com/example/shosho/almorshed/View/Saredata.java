8567366874e8ac34b22cd0cdd960a1c54ddc7034
AlshimaaRifaat <alshimaa.rifaat71@gmail.com>
2019-01-06 16:28:55 +0200

    all except api

---
+++ app/src/main/java/com/example/shosho/almorshed/View/Saredata.java
@@ -0,0 +1,10 @@
+package com.example.shosho.almorshed.View;
+
+import com.example.shosho.almorshed.model.Quran;
+
+import java.util.List;
+
+public interface Saredata {
+
+    void listquran(Quran list);
+}
